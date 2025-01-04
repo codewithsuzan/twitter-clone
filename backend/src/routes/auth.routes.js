834df78798a8bf5ea6db409e4f06e03b38259a01
Sujan Timalsina <sujantimalsina333@gmail.com>
2025-01-04 17:40:30 +0545

    /me route added !!!

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,5 +1,6 @@
 import express from "express";
-import { signup, login, logout } from "../controllers/auth.controller.js";
+import { signup, login, logout,getMe } from "../controllers/auth.controller.js";
+import { protectRoute } from "../middlewares/protectRoute.js";
 
 const router = express.Router();
 
@@ -7,5 +8,6 @@ const router = express.Router();
 router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);
+router.post("/me",protectRoute,getMe);
 
 export default router;
