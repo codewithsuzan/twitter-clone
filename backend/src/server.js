6471d3f00a0768ac126a1ab126109ba1390ff0ef
Sujan Timalsina <sujantimalsina333@gmail.com>
2025-01-08 17:29:43 +0545

    server.js modified!!!

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,25 +1,38 @@
 import express from "express";
 import dotenv from "dotenv";
-import authRoute from "./routes/auth.routes.js";
-import connectDB from "./db/db.js";
+import { v2 as cloudinary } from "cloudinary";
 import cookieParser from "cookie-parser";
+
+import connectDB from "./db/db.js";
+
+import authRoute from "./routes/auth.routes.js";
+import userRoute from "./routes/user.routes.js";
+import postRoute from "./routes/post.routes.js";
 
 const app = express();
 
 dotenv.config();
 
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
 app.use(express.json()); // to parse req.body
-app.use(cookieParser())
+app.use(cookieParser());
 
 app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/posts", postRoute);
 
 app.use(express.urlencoded({ extended: true })); // to parse x-www-form-urlencoded
 connectDB()
-.then(() => {
-  app.listen(process.env.PORT, () => {
-    console.log(`\n🛞  Server is running at port:${process.env.PORT}`);
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log(`\n🛞  Server is running at port:${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("MONGO db connection failed !!!", error);
   });
-})
-.catch((error) => {
-  console.log("MONGO db connection failed !!!", error);
-});
