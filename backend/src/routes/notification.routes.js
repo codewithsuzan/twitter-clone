795350c48b4d57ab92ac6a197633490cbb9b3ffc
Sujan Timalsina <sujantimalsina333@gmail.com>
2025-01-09 21:04:16 +0545

    notification route completed!!!

---
+++ backend/src/routes/notification.routes.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import { protectRoute } from '../middlewares/protectRoute.js';
+import {
+    getNotifications,
+    deleteNotifications,
+    deleteOneNotification,
+} from "../controllers/notification.controller.js"
+
+const router=express.Router()
+
+router.get('/',protectRoute,getNotifications)
+router.delete('/',protectRoute,deleteNotifications)
+
+
+export default router
