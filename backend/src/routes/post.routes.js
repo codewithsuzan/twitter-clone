c8c2a4ececde42ca896f8ab2903c3addb23a3ef5
Sujan Timalsina <sujantimalsina333@gmail.com>
2025-01-09 20:26:00 +0545

    post route completed!!!

--- backend/src/routes/post.routes.js
+++ backend/src/routes/post.routes.js
@@ -1,15 +1,25 @@
 import express from "express";
 import { protectRoute } from "../middlewares/protectRoute.js";
 import {
-    createPost
-} from "../controllers/post.controller.js"
+  createPost,
+  deletePost,
+  likeUnlikePost,
+  commentOnPost,
+  getAllPosts,
+  getLikedPosts,
+  getFollowingPost,
+  getUserPosts,
+} from "../controllers/post.controller.js";
 
 const router = express.Router();
 
-router.post("/create",protectRoute,createPost)
-// router.delete("/delete",protectRoute,deletePost)
-// router.post("/like/:id",protectRoute,likeUnlikePost)
-// router.post("/comment/:id",protectRoute,commentPost)
+router.get("/user/:username", protectRoute, getUserPosts);
+router.get("/following", protectRoute, getFollowingPost);
+router.get("/all", protectRoute, getAllPosts);
+router.get("/like:id", protectRoute, getLikedPosts);
+router.post("/create", protectRoute, createPost);
+router.delete("/:id", protectRoute, deletePost);
+router.post("/like/:id", protectRoute, likeUnlikePost);
+router.post("/comment/:id", protectRoute, commentOnPost);
 
-
-export default router
+export default router;
