8f5d26e6bab7d540c91c89c3a3c9f32318272b88
Sujan Timalsina <sujantimalsina333@gmail.com>
2025-01-09 20:24:52 +0545

    user model completed!!!

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -52,6 +52,13 @@ const userShhema = new Schema(
       type: String,
       default: "",
     },
+    likedPosts: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Post",
+        default: [],
+      }
+    ]
   },
   {
     timestamps: true,
