c5f90ab24faf587211f5806872d7ed8107039f2e
Sujan Timalsina <sujantimalsina333@gmail.com>
2025-01-14 13:00:16 +0545

    New pages created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,15 +3,20 @@ import { Route, Routes } from "react-router-dom";
 import SignUpPage from "./pages/auth/signup/SignUpPage";
 import HomePage from "./pages/home/HomePage";
 import LoginPage from "./pages/auth/login/LoginPage";
+import Sidebar from "./components/common/Sidebar";
+import RightPanel from "./components/common/RightPanel";
 
 function App() {
   return (
     <div className="flex max-w-6xl mx-auto">
+      {/* Common components,because it's not wrapped with Routes */}
+      <Sidebar/>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/signup" element={<SignUpPage />} />
         <Route path="/login" element={<LoginPage />} />
       </Routes>
+      <RightPanel/>
     </div>
   );
 }
